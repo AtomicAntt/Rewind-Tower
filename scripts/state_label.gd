11fb8d89489d4c5ef824eb9d837b220883b4c21e
AtomicAntt <anthonychi36@gmail.com>
2026-03-21 14:45:19 -0700

    Update collision on troops to try to ignore coin on wrist and try and fix win condition.

--- scripts/state_label.gd
+++ scripts/state_label.gd
@@ -9,5 +9,5 @@ func _physics_process(_delta: float) -> void:
 		text = "Intermission (Round " + str(RoundManager.current_round) + ")"
 	elif RoundManager.is_gameover():
 		text = "Game Over! (Round " + str(RoundManager.current_round) + ")"
-	elif RoundManager.is_game_over():
+	elif RoundManager.is_won():
 		text = "Congratulations, you win!"

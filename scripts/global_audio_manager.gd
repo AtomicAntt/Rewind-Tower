b1a8c99ce654007a931099ee98f58540749aa334
Effiayre <Effiayre@gmail.com>
2026-03-21 17:35:43 -0700

    Add music change on round state

--- scripts/global_audio_manager.gd
+++ scripts/global_audio_manager.gd
@@ -7,11 +7,25 @@ extends Node
 @export var round_start_audio: AudioStreamPlayer
 @export var round_win_audio: AudioStreamPlayer
 
+@export var intermission_music: AudioStreamPlayer
+@export var gametime_music: AudioStreamPlayer
+
 func _ready() -> void:
 	RoundManager.game_over.connect(play_game_over)
 	RoundManager.round_won.connect(play_round_won)
 	RoundManager.round_started.connect(play_round_started)
 
+func _process(_delta: float) -> void:
+	if RoundManager.in_intermission() and !intermission_music.playing:
+		intermission_music.play()
+	elif RoundManager.in_battle() and !gametime_music.playing and !round_start_audio.playing:
+		gametime_music.play()
+		
+	if round_start_audio.playing:
+		intermission_music.stop()
+	if round_win_audio.playing:
+		gametime_music.stop()
+		
 func play_game_over() -> void:
 	game_over_audio.play()
 

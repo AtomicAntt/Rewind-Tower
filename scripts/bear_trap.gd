c0ab145ff3674ebdf5533828d2935dfc58dd6af9
AtomicAntt <anthonychi36@gmail.com>
2026-03-21 21:19:43 -0700

    Add bear and bear trap audio

--- scripts/bear_trap.gd
+++ scripts/bear_trap.gd
@@ -27,6 +27,15 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 			enemy.hurt(enemy.max_enemy_hp*0.2)
 			animation_player.queue(shut_animation)
 			enemy_trapping = enemy
+			var sound_to_play: int = randi_range(1, 3)
+			match sound_to_play:
+				1:
+					$Audio1.play()
+				2:
+					$Audio2.play()
+				3:
+					$Audio3.play()
+			
 			$Timer.start()
 	if area.is_in_group("Drawer"):
 		xray.visible = true

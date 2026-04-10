5b894487bfbca21c051f2844b4666f590bf32793
Effiayre <Effiayre@gmail.com>
2026-04-10 12:58:38 -0700

    Fix Knob Interactable Wrap Around

--- scripts/knob_interactable.gd
+++ scripts/knob_interactable.gd
@@ -7,6 +7,9 @@ var controller: XRController3D
 var position: Vector3
 var rotation: float
 
+var controller_rot: float
+var prev_controller_rot: float = 0.0
+
 var picked_up_rotation: float
 
 var value: float = 0.0
@@ -25,23 +28,30 @@ func _process(_delta: float) -> void:
 	pickable.position = position
 	pickable.rotation = Vector3(0,0,rotation)
 	
+	
 	if pickable.is_picked_up():
 		has_been_let_go = false
 		
 		controller = pickable.get_picked_up_by_controller()
+		
+		controller_rot = controller.rotation.z
+		
+		var controller_rot_diff = controller_rot - prev_controller_rot
+		
+		#Fix Wraparound
+		if controller_rot_diff > PI:
+			controller_rot -= TAU
+		elif controller_rot_diff < -PI:
+			controller_rot += TAU
+		
+		prev_controller_rot = controller_rot
 		
 		#Called once per pickup
 		if not has_been_picked_up:
 			_on_picked_up()
 			has_been_picked_up = true
 		
-		delta_rot = controller.rotation.z - picked_up_rotation
-		
-		#Fix wraparound
-		if delta_rot > 180:
-			delta_rot -= 360
-		elif delta_rot < -180:
-			delta_rot += 360
+		delta_rot = controller_rot - picked_up_rotation
 		
 		delta_rot = clampf(delta_rot, -10, 10)
 		
@@ -49,7 +59,7 @@ func _process(_delta: float) -> void:
 		
 		value = -delta_rot
 		
-		print(delta_rot)
+		#print(delta_rot)
 		
 	else:
 		has_been_picked_up = false
@@ -57,8 +67,7 @@ func _process(_delta: float) -> void:
 		#Called once per let go
 		if not has_been_let_go:
 			prev_rotation += (delta_rot - prev_rotation)
-			#print(prev_rotation)
 			has_been_let_go = true
 		
 func _on_picked_up() -> void:
-	picked_up_rotation = controller.rotation.z - prev_rotation
+	picked_up_rotation = controller_rot - prev_rotation

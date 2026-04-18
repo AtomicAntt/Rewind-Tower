dc0cb1297676c69ce6e53f966d39648f01a3491d
AtomicAntt <anthonychi36@gmail.com>
2026-04-18 15:08:12 -0700

    Sync catapult animations

--- scripts/catapult.gd
+++ scripts/catapult.gd
@@ -5,6 +5,7 @@ extends DefenseTroop
 @export var anim_catapult_launch: String
 @export var anim_frame_launch: String
 @export var anim_rock_launch: String
+@export var anim_rock_reset: String
 
 @onready var shoot_position: Marker3D = %ShootPosition
 
@@ -24,15 +25,23 @@ func _on_shoot_timer_timeout() -> void:
 ## This will lead to firing a projectile as well, since anim_rock_launch will call fire_projectile()
 func play_shoot_animation() -> void:
 	animator.queue(anim_catapult_launch)
-	animator.queue(anim_rock_launch)
-	if is_instance_valid(closest_enemy_area):
-		var enemy_target: Enemy = closest_enemy_area.get_parent()
-		fire_projectile(enemy_target.global_position)
-	animator.queue(anim_frame_launch)
+	#if is_instance_valid(closest_enemy_area):
+		#var enemy_target: Enemy = closest_enemy_area.get_parent()
+		#fire_projectile(enemy_target.global_position)
+	#animator.queue(anim_frame_launch)
+	animator.queue(anim_rock_reset)
 
-## This should get called at the start of anim_rock_launch to have a synced animation
-func fire_projectile(global_enemy_position: Vector3) -> void:
-	# First, lets calculate the time it would take for the projectile to hit the floor (Y AXIS)
+## This should get called in the AnimationPlayer in Catapult Launch right when the catapult handle hits the cross beam.
+func fire_projectile() -> void:
+	# First, get our global enemy position if one exists, else it exits this function.
+	var global_enemy_position: Vector3
+	if not is_instance_valid(closest_enemy_area):
+		return
+	else:
+		var enemy_target: Enemy = closest_enemy_area.get_parent()
+		global_enemy_position = enemy_target.global_position
+	
+	# Then, lets calculate the time it would take for the projectile to hit the floor (Y AXIS)
 	# We are using this formula: displacement_y = v_iy*t + 1/2*a_y*t^2
 	# We know initial velocity (y) = 0 and a_y = gravity (acceleration)
 	# t = sqrt(2*displacement/a)
@@ -41,7 +50,7 @@ func fire_projectile(global_enemy_position: Vector3) -> void:
 	
 	# Now, we figure out the initial velocity in the horizontal axis (Z & X)
 	
-	# First, we get the horizontal displacement
+	# So, we get the horizontal displacement
 	var shooting_horizontal_position: Vector3 = Vector3(shoot_position.global_position.x, 0, shoot_position.global_position.z)
 	var enemy_horizontal_position: Vector3 = Vector3(global_enemy_position.x, 0, global_enemy_position.z)
 	var horizontal_displacement: float = shooting_horizontal_position.distance_to(enemy_horizontal_position)
@@ -59,10 +68,3 @@ func fire_projectile(global_enemy_position: Vector3) -> void:
 	projectile_instance.set_damage(attack_damage)
 	projectile_instance.set_stream(%ArrowHit.stream)
 	projectile_instance.set_forward_speed(initial_horizontal_speed)
-
-
-#func _on_animation_player_animation_changed(_old_name: StringName, new_name: StringName) -> void:
-	#if new_name == anim_rock_launch:
-		#if is_instance_valid(closest_enemy_area):
-			#var enemy_target: Enemy = closest_enemy_area.get_parent()
-			#fire_projectile(enemy_target.global_position)

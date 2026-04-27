b2899af1b4f3f37ea6a90a05e441bb5546dd2e26
Effiayre <Effiayre@gmail.com>
2026-04-27 13:43:50 -0700

    Add New Vertical Table Adjustment System
    This does not use an XRToolsSlider. Using an XRToolsSlider causes far too many issues.

--- scripts/settings_handler.gd
+++ scripts/settings_handler.gd
@@ -44,7 +44,11 @@ func set_table_height(value: float):
 	config.set_value("GENERAL","table_height",value)
 
 func get_table_height() -> float:
+<<<<<<< Updated upstream
 	return config.get_value("GENERAL","table_height", 0)
+=======
+	return config.get_value("GENERAL","table_height", .8)
+>>>>>>> Stashed changes
 
 
 func _ready() -> void:
